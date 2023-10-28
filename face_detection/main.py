514e78d8002370a56b9b39a1a5abecc1ef273952
fernandocalenzani <fernandocalenzani@gmail.com>
2023-10-27 22:32:59 -0300

    first commit

---
+++ face_detection/main.py
@@ -0,0 +1,13 @@
+  """
+  FACE DETECTION
+  """
+
+  """
+  1. Haarcascade and OpenCV
+  2. Eyes, Face, People and Clock
+  3. HOG and Dlib
+  4. Face detection using CNN using Dlib
+  5. Face detection using webcam
+  """
+
+
---
+++ face_detection/readme.md
@@ -0,0 +1,10 @@
+<h1>FACE DETECTION<h1>
+
+<h2>Summary<h2>
+1. Haarcascade and OpenCV
+2. Eyes, Face, People and Clock
+3. HOG and Dlib
+4. Face detection using CNN using Dlib
+5. Face detection using webcam
+
+<h2>RESUME<h2>
