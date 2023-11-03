de0c26624e4cc41c2c9bf441617ea2b4b706e186
fernandocalenzani <fernandocalenzani@gmail.com>
2023-11-02 22:26:42 -0300

    feat: neural network multilayers

--- app/src/classify_images_nn/nn_multlayer.py
+++ app/src/classify_images_nn/nn_multlayer.py
@@ -1,24 +1,82 @@
 import numpy as np
 
+"""
+1. Inicialização:
+    Inicialize a estrutura da rede neural, incluindo o número de camadas, o número de neurônios em cada camada, os pesos iniciais e as funções de ativação.
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+2. Forward Pass (Propagação Direta):
+    Alimente os dados de entrada na camada de entrada.
+    Para cada camada oculta e a camada de saída:
+    Calcule a soma ponderada das entradas multiplicadas pelos pesos.
+    Aplique a função de ativação à soma ponderada para obter as saídas da camada.
+
+3. Cálculo do Erro:
+Compare as saídas da rede com os valores alvo para calcular o erro. Isso é feito usando uma função de erro, como o erro quadrático médio (MSE).
+
+4. Backward Pass (Retropropagação):
+    Propague o erro da camada de saída para as camadas anteriores.
+    Atualize os pesos em cada camada para reduzir o erro. Isso é feito usando algoritmos de otimização, como o Gradiente Descendente.
+    Ajuste os pesos com base nas derivadas dos erros em relação aos pesos (gradiente) usando a regra da cadeia.
+
+5. Repetição:
+    Repita as etapas 2 a 4 para várias épocas de treinamento até que o erro atinja um valor aceitável ou até que a rede convirja.
+
+6. Predição:
+Use a rede neural treinada para fazer previsões em novos dados não vistos.
+
+"""
 
 
-def perceptron(input_data, weights, bias):
-    activation = np.dot(input_data, weights) + bias
-    output = sigmoid(activation)
-    return output
+import numpy as np
+import sympy as sp
 
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+layers = [
+    np.array([
+        [-0.424, 0.358],
+        [-0.740, -0.577],
+        [-0.961, -0.469]
+    ]),
+    np.array([-0.017, -0.893, 0.148])
+]
+Yexp = [0, 1, 1, 0]
+Y = []
 
-inputs = np.array([(0, 0), (0, 1), (1, 0), (1, 1)])
-weights = np.array([(0.5, 0.5), (0.5, 0.5), (0.5, 0.5), (0.5, 0.5)])
-bias = 0
 err = 100
-result = []
+Err = []
 
-for i in range(0, len(inputs), 1):
-    output = perceptron(inputs[i], weights[i], bias)
-    result.append(output)
+u = sp.symbols('u')
+f = 1 / (1 + sp.exp(-u))
+moment = 1
 
-print("Saída do perceptron:", result)
+
+def calc_delta(err, u_value):
+    dydx = sp.diff(f, u)
+    return err*dydx.subs(u, u_value)
+
+
+def calc_delta_hide_layer(dydx_f, w, dOut):
+    return dydx_f * w * dOut
+
+
+def f_sum(inputs, weights):
+    outputs = inputs
+    for layer_weights in weights:
+        summ = np.dot(layer_weights, outputs)
+        outputs = np.vectorize(lambda x: f.subs(u, x))(summ)
+    return outputs
+
+
+for i in range(0, len(X), 1):
+    y = f_sum(X[i], layers)
+    err = y - Yexp[i]
+
+    delta = calc_delta(err, y)
+    delta_hide = calc_delta_hide_layer()
+    print(delta, err, y)
+
+    Err.append(err)
+    Y.append(y)
+
+""" print(Y)
+print(Err) """
--- app/src/classify_images_nn/readme.md
+++ app/src/classify_images_nn/readme.md
@@ -4,4 +4,58 @@
 
 Here are some of the key topics I will be covering during this study project:
 
-- Extrac characteristic
+```
+01. Coleta e Preparação de Dados
+02. Pré-processamento de Dados
+03. Escolha da Arquitetura da Rede Neural
+04. Inicialização de Parâmetros
+05. Feedforward (Propagação Direta)
+06. Cálculo da Função de Ativação
+07. Cálculo da Função de Custo
+08. Retropropagação (Backpropagation)
+09. Treinamento da Rede Neural
+10. Avaliação do Modelo
+11. Ajuste e Otimização
+12. Implantação
+```
+
+## Base
+
+1. Inicialização:
+   Inicialize a estrutura da rede neural, incluindo o número de camadas, o número de neurônios em cada camada, os pesos iniciais e as funções de ativação.
+
+2. Forward Pass (Propagação Direta):
+   Alimente os dados de entrada na camada de entrada.
+   Para cada camada oculta e a camada de saída:
+   Calcule a soma ponderada das entradas multiplicadas pelos pesos.
+   Aplique a função de ativação à soma ponderada para obter as saídas da camada.
+
+3. Cálculo do Erro:
+   Compare as saídas da rede com os valores alvo para calcular o erro. Isso é feito usando uma função de erro, como o erro quadrático médio (MSE).
+
+4. Backward Pass (Retropropagação):
+   Propague o erro da camada de saída para as camadas anteriores.
+   Atualize os pesos em cada camada para reduzir o erro. Isso é feito usando algoritmos de otimização, como o Gradiente Descendente.
+   Ajuste os pesos com base nas derivadas dos erros em relação aos pesos (gradiente) usando a regra da cadeia.
+
+5. Repetição:
+   Repita as etapas 2 a 4 para várias épocas de treinamento até que o erro atinja um valor aceitável ou até que a rede convirja.
+
+6. Predição:
+   Use a rede neural treinada para fazer previsões em novos dados não vistos.
+
+- Bias: additional neuron to improve the performance
+
+### MEAN SQUARE ERROR (MSE)
+
+- MSE = (1/n) . SUM(fi - yi)^2
+
+### ROOT MEAN SQUARE ERROR (RMSE)
+
+- RMSE = ((1/n) . SUM(fi - yi)^2)^1/2
+
+### PARAMS
+
+- Learning Rate
+- Batch Size
+- Epochs
